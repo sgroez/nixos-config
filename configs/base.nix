57a856546904e854d89c13e559614ddd472ec881
venticx <venticx@gmail.com>
2025-06-06 19:34:56 +0200

    created fresh flake with multi platform installation medium and configuration

--- configs/base.nix
+++
@@ -1,7 +0,0 @@
-{ system, customModules }:inputs.nixpkgs.lib.nixosSystem {
-  inherit system;
-  modules = [
-    ../shared/configuration.nix
-    (import "${customModules}/basepackages.nix")
-  ];
-}
