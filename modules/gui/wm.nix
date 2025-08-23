3ee0f7acc79d63c1b818b9f68e4b8dfc94d8dfc2
venticx <venticx@gmail.com>
2025-08-23 16:40:32 +0200

    switched from ly to sddm and added svkbd virtual keyboard

--- modules/gui/wm.nix
+++ modules/gui/wm.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }: {
   programs.hyprland.enable = true;
 
-  services.displayManager.ly.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
   environment.systemPackages = with pkgs; [
     brightnessctl
