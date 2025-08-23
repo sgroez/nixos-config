9d1111aa2b2d57732b899bba6b93b0b718e32651
venticx <venticx@gmail.com>
2025-08-23 14:16:13 +0200

    removed platforms as they moved to hosts and added ly login manager

--- modules/gui/wm.nix
+++ modules/gui/wm.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   programs.hyprland.enable = true;
 
+  services.displayManager.ly.enable = true;
+
   environment.systemPackages = with pkgs; [
     brightnessctl
     pavucontrol
--- platforms/pi.nix
+++
@@ -1,9 +0,0 @@
-{
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_SD";
-    fsType = "ext4";
-  };
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = false; # Not needed on ARM either
-  boot.loader.generic-extlinux-compatible.enable = true;
-}
