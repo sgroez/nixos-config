a12b5038b3bcb2b026ece0044a49c0d2f3f3a2db
venticx <venticx@gmail.com>
2026-01-11 13:04:28 +0100

    added flake and nix command experimental features to mac config

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -5,6 +5,7 @@
     ../../modules/base/basepackages.nix
   ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
 }
