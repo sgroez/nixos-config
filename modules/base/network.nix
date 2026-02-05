df6aede84f62b577e1c0d854263c3122e85296a5
venticx <venticx@gmail.com>
2026-02-05 13:36:08 +0100

    switched network module to use resolved service to match new networkmanager default dns config

--- modules/base/network.nix
+++ modules/base/network.nix
@@ -1,5 +1,6 @@
 {
   networking.networkmanager.enable = true;
+  services.resolved.enable = true;
   networking.firewall.enable = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ 22 ];
