5a37425e8fa7f6a0dcba2cb525895e902d6f8925
sgroez <venticx@gmail.com>
2026-06-22 08:59:32 +0200

    added firefox privacy settings and extensions for vim keybinds and add block to gui module

--- modules/profiles/gui.nix
+++ modules/profiles/gui.nix
@@ -7,6 +7,14 @@
 let
   cfg = config.profiles.gui;
   inherit (lib) mkEnableOption mkIf mkMerge;
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
 in
 {
   options.profiles.gui = {
@@ -28,9 +36,67 @@ in
         fuzzel
         quickshell
         hyprshot
-        firefox
         yubioath-flutter
       ];
+
+      # taken from https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
+      programs.firefox = {
+        enable = true;
+        policies = {
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          EnableTrackingProtection = {
+            Value= true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          DisablePocket = true;
+          DisableFirefoxAccounts = true;
+          DisableAccounts = true;
+          DisableFirefoxScreenshots = true;
+          OverrideFirstRunPage = "";
+          OverridePostUpdatePage = "";
+          DontCheckDefaultBrowser = true;
+          DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+          DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+          SearchBar = "unified"; # alternative: "separate"
+          ExtensionSettings = {
+            "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+            # uBlock Origin:
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # vimium
+            "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+              installation_mode = "force_installed";
+            };
+          };
+          Preferences = { 
+            "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+            "extensions.pocket.enabled" = lock-false;
+            "extensions.screenshots.disabled" = lock-true;
+            "browser.topsites.contile.enabled" = lock-false;
+            "browser.formfill.enable" = lock-false;
+            "browser.search.suggest.enabled" = lock-false;
+            "browser.search.suggest.enabled.private" = lock-false;
+            "browser.urlbar.suggest.searches" = lock-false;
+            "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+            "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+            "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+            "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+            "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+            "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+            "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+            "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+            "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+          };
+        };
+      };
+
       services.pcscd.enable = true;
 
       services.printing.enable = true;
